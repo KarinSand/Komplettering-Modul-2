8d50eda9e78b960db4e6b98534a9f5c45e3d043d
KarinSand <karen.h.sand@gmail.com>
2026-06-07 22:23:19 +0200

    Add local storage vporperly

--- script.js
+++ script.js
@@ -2,6 +2,11 @@ const apiKey = "9d11cfb88ff8a2357116587a4d2ea061";
 let history = [];
 $(document).ready(function () {
 
+    const savedHistory = localStorage.getItem("weatherHistory");
+    if (savedHistory !== null) {
+        history = JSON.parse(savedHistory);
+        showHistory();
+    }
     // Sökfunktion
     $("#searchForm").on("submit", function (event) {
         event.preventDefault();
@@ -10,9 +15,9 @@ $(document).ready(function () {
             $("#weather-view").html("<p>Skriv in en stad först.</p>");
             return;
         }
-
         $("#weather-view").html("<p>Hämtar väder för " + city + "...</p>");
         getWeatherCity(city);
+        $("#cityInput").val("");
     });
     // My location-knapp
     $("#locationBtn").on("click", function () {
@@ -51,13 +56,19 @@ function getWeatherCity(city) {
 // Visa vädret
 function showWeather(data) {
 
+    const iconName = data.weather[0].icon;
+    const iconUrl = "https://openweathermap.org/img/wn/" + iconName + "@2x.png";
     const html = `
-        <h2>${data.name}</h2>
-        <p>Temperatur: ${data.main.temp} °C</p>
-        <p>Vind: ${data.wind.speed} m/s</p>
-        <p>${data.weather[0].description}</p>
+        <div class="weather-card">
+            <img src="${iconUrl}" alt="${data.weather[0].description}">
+            <h2>${data.name}</h2>
+            <p>Temperatur: ${data.main.temp} °C</p>
+            <p>Vind: ${data.wind.speed} m/s</p>
+            <p>${data.weather[0].description}</p>
+        </div>
     `;
-    $("#weather-view").html(html);
+
+    $("#weather-view").html(html); // visa vädret i #weather-view
 }
 // Hämta väder med koordinater
 function getWeatherLocation(lat, lon) {
@@ -66,7 +77,6 @@ function getWeatherLocation(lat, lon) {
 
     $.getJSON(url)
         .done(function (data) {
-
             showWeather(data);
             addToHistory(data);
         })
@@ -78,6 +88,7 @@ function getWeatherLocation(lat, lon) {
 }
 // Lägg till historik
 function addToHistory(data) {
+
     const search = {
         city: data.name,
         temp: data.main.temp,
@@ -87,7 +98,9 @@ function addToHistory(data) {
     if (history.length > 5) {
         history.pop();
     }
-    showHistory();}
+    localStorage.setItem("weatherHistory", JSON.stringify(history));
+    showHistory();
+}
 // Visa historik
 function showHistory() {
     let html = "<h2>Senaste sökningar</h2>";
