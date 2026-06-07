2c3c9f4281ab8f0d95d1b6808cd028a48ac5f478
KarinSand <karen.h.sand@gmail.com>
2026-06-07 21:30:40 +0200

    layout styles

--- script.js
+++ script.js
@@ -4,44 +4,34 @@ $(document).ready(function () {
     //testing
 
     $("#searchForm").on("submit", function (event) {
-
         event.preventDefault();
-
         const city = $("#cityInput").val().trim();
-
         if (city === "") {
             $("#weather-view").html("<p>Skriv in en stad först.</p>");
             return;
         }
-
         $("#weather-view").html("<p>Hämtar väder för " + city + "...</p>");
-
         getWeatherCity(city);
     });
 
 });
 
 function getWeatherCity(city) {
-
     const url =
         `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&lang=sv&appid=${apiKey}`;
 
     $.getJSON(url)
         .done(function(data) {
-
             showWeather(data);
-
         });
 
 }
 function showWeather(data) {
-
     const html = `
         <h2>${data.name}</h2>
         <p>Temperatur: ${data.main.temp} °C</p>
         <p>${data.weather[0].description}</p>
     `;
-
     $("#weather-view").html(html);
 
 }
